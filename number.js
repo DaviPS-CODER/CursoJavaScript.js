6981078500f15deb538f793be62497ae6934f24b
Davi Pereira <davi.160402@gmail.com>
2022-03-05 02:26:12 -0300

    meus estudos

---
+++ number.js
@@ -0,0 +1,12 @@
+const n1 = 1.0                    // 1.0 pra JS é inteiro
+const n2 = Number("2.9")
+console.log(Number.isInteger(n2)) // pra ver se é inteiro 
+
+const avaliacao1 = 9.23
+
+const total = n1 * avaliacao1
+
+console.log(total.toFixed(2)) //fixa as casas decimais
+console.log(total.toString()) // transforma em string em binário
+console.log(typeof total)
+
